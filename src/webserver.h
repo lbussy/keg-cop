83ee642d5208a15c4cb2a14e0da531dc41293691
Lee Bussy <lee@bussy.org>
2020-04-19 14:11:55 -0500

    Replacement files

---
+++ src/webserver.h
@@ -0,0 +1,52 @@
+/* Copyright (C) 2019-2020 Lee C. Bussy (@LBussy)
+
+This file is part of Lee Bussy's Brew Bubbbles (brew-bubbles).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+#ifndef _WEBSERVER_H
+#define _WEBSERVER_H
+
+#include "wifihandler.h"
+#include "jsonconfig.h"
+#include "version.h"
+#include "config.h"
+#include <ArduinoLog.h>
+#include <ArduinoJson.h>
+#include <AsyncJson.h>
+#include <FS.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <HTTPClient.h>
+#include <ESPmDNS.h>
+#include <Arduino.h>
+
+void initWebServer();
+void setRegPageAliases();
+void setActionPageHandlers();
+void setJsonHandlers();
+void setSettingsAliases();
+void stopWebServer();
+
+extern struct Config config;
+// extern struct ThatVersion thatVersion;
+extern const size_t capacityDeserial;
+extern const size_t capacitySerial;
+
+#endif // _WEBSERVER_H
