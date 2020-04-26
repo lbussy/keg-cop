e01b80a49a9c76abe7ad3ab0647caf28bb71b1fa
Lee Bussy <lee@bussy.org>
2020-04-26 18:08:42 -0500

    Add in default AP password

--- src/config.h
+++ src/config.h
@@ -172,7 +172,7 @@ SOFTWARE. */
 // Access point password
 //
 #ifndef AP_PASSWD
-#define AP_PASSWD ""
+#define AP_PASSWD "kegcop"
 #endif
 //
 //////////////////////////////////////////////////////////////////////////
