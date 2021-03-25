437d2cd6cceae27c8d87c7b50a0272fac2a1061a
Lee Bussy <lee@bussy.org>
2021-03-25 18:14:37 -0500

    Add MQTT code

---
+++ src/mqttclient.h
@@ -0,0 +1,40 @@
+/* Copyright (C) 2019-2021 Lee C. Bussy (@LBussy)
+
+This file is part of Lee Bussy's Keg Cop (keg-cop).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+#ifndef _MQTTCLIENT_H
+#define _MQTTCLIENT_H
+
+#include "jsonconfig.h"
+
+#include <AsyncMqttClient.h>
+#include <ArduinoLog.h>
+#include <Ticker.h>
+
+void setupMqtt();
+void connectMqtt();
+void disconnectMqtt();
+bool sendPulsesMqtt(int * tapID, unsigned int * pulses);
+void onMqttConnect(bool sessionPresent);
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+void onMqttPublish(uint16_t packetId);
+
+#endif // _MQTTCLIENT_H
