37ebdd1e51e2923476d397732b00bf9c4650c5b0
LBussy <lee@bussy.org>
2022-07-16 13:15:40 -0500

    Move alerts to common

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
 # TODO:
 
 - [ ] Fix SPIFFS Editor
+- [ ] Maybe add flowcal to kegcop.js?
+- [ ] Maybe move pollComplete() to kegcop.js
--- data/kegcop.js
+++ data/kegcop.js
@@ -155,3 +155,50 @@ function toggleLoader(status) {
         }
     }
 }
+
+// AJAX errors
+// Used in index, settings, temps
+
+tempAlert = function () { }
+tempAlert.warning = function (message) {
+    _div = "";
+    if (message) {
+        _div = '<div class="alert alert-dismissible alert-warning" id="warnTempError">'
+        _div += '<button type="button" class="close" data-dismiss="alert">&times;</button><p class="mb-0">'
+        _div += '<strong>Temps Warning: </strong><span>' + message + '</span></p></div>'
+    }
+    $('#flowAlert_placeholder').html(_div);
+}
+
+configAlert = function () { }
+configAlert.warning = function (message) {
+    _div = "";
+    if (message) {
+        _div = '<div class="alert alert-dismissible alert-warning" id="warnConfigError">'
+        _div += '<button type="button" class="close" data-dismiss="alert">&times;</button><p class="mb-0">'
+        _div += '<strong>Config Warning: </strong><span>' + message + '</span></p></div>'
+    }
+    $('#flowAlert_placeholder').html(_div);
+}
+
+flowAlert = function () { }
+flowAlert.warning = function (message) {
+    _div = "";
+    if (message) {
+        _div = '<div class="alert alert-dismissible alert-warning" id="warnFlowError">'
+        _div += '<button type="button" class="close" data-dismiss="alert">&times;</button><p class="mb-0">'
+        _div += '<strong>Flow Warning: </strong><span>' + message + '</span></p></div>'
+    }
+    $('#flowAlert_placeholder').html(_div);
+}
+
+settingsAlert = function () { }
+settingsAlert.error = function (message) {
+    _div = "";
+    if (message) {
+        _div = '<div class="alert alert-dismissible alert-danger" id="warnSettingsError">'
+        _div += '<button type="button" class="close" data-dismiss="alert">&times;</button><p class="mb-0">'
+        _div += '<strong>Settings Error: </strong><span>' + message + '</span></p></div>'
+    }
+    $('#settingsAlert_placeholder').html(_div);
+}
