5170b686b3cbef5b88857de4e39cde57402fcf47
LBussy <lee@bussy.org>
2022-08-21 08:46:26 -0600

    Change fan off icon

--- data/index.js
+++ data/index.js
@@ -170,7 +170,7 @@ function populateTemps(callback = null) { // Get current temperature and state
                         // Set tower fan indicator
                         switch (temps.tfanstate) {
                             case 0: // TSTAT_INACTIVE
-                                $('#towerFan').html('<i class="fa-solid fa-ban"></i>');
+                                $('#towerFan').html('<i class="fa-regular fa-circle-pause"></i>');
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan is off.");
                                 break;
                             case 1: // TSTAT_COOL_BEGIN
@@ -178,7 +178,7 @@ function populateTemps(callback = null) { // Get current temperature and state
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan is on.");
                                 break;
                             case 2: // TSTAT_COOL_MINOFF
-                                $('#towerFan').html('<i class="fa-solid fa-ban"></i>');
+                                $('#towerFan').html('<i class="fa-regular fa-circle-pause"></i>');
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan is off.");
                                 break;
                             case 3: // TSTAT_COOL_ACTIVE
@@ -186,7 +186,7 @@ function populateTemps(callback = null) { // Get current temperature and state
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan is on.");
                                 break;
                             case 4: // TSTAT_IDLE_END
-                                $('#towerFan').html('<i class="fa-solid fa-ban"></i>');
+                                $('#towerFan').html('<i class="fa-regular fa-circle-pause"></i>');
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan is off.");
                                 break;
                             case 5: // TSTAT_IDLE_MINON
@@ -194,16 +194,16 @@ function populateTemps(callback = null) { // Get current temperature and state
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan is on.");
                                 break;
                             case 6: // TSTAT_IDLE_INACTIVE
-                                $('#towerFan').html('<i class="fa-solid fa-ban"></i>');
+                                $('#towerFan').html('<i class="fa-regular fa-circle-pause"></i>');
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan is off.");
                                 break;
                             case 7: // TSTAT_UNKNOWN
-                                $('#towerFan').html('<i class="fa-solid fa-ban"></i>');
+                                $('#towerFan').html('<i class="fa-regular fa-circle-pause"></i>');
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan state unknown.");
                                 console.log("DEBUG: Tower state unknown (7).");
                                 break;
                             default: // TSTAT_UNKNOWN
-                                $('#towerFan').html('<i class="fa-solid fa-ban"></i>');
+                                $('#towerFan').html('<i class="fa-regular fa-circle-pause"></i>');
                                 $("#fanstatetooltip").attr("data-original-title", "Tower fan state unknown.");
                                 console.log("DEBUG: Tower state unknown (default).");
                                 break;
