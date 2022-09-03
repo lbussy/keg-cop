6622aca7e3811478e0e2ef7ad4536f728fe40474
LBussy <lee@bussy.org>
2022-09-03 13:01:43 -0500

    Add base pages

---
+++ data/bulkload.js
@@ -0,0 +1,10 @@
+// Supports About Page
+
+toggleLoader("off");
+var numReq = 0 + numReqPre;
+var loaded = 0;
+
+function finishLoad() {
+    // Catch page finished event from kegcop_pre.js
+    // TODO:  Decide if we need any "on load" processing here.
+}
