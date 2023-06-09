883a48d6c8f460c92849b44d53c43592f1921b9e
LBussy <lee@bussy.org>
2023-06-09 15:32:48 -0500

    Move tempsensor defines out of headers

--- src/tempsensors.cpp
+++ src/tempsensors.cpp
@@ -22,6 +22,11 @@ SOFTWARE. */
 
 #include "tempsensors.h"
 
+#include "appconfig.h"
+
+#include <OneWire.h>
+#include <DS18B20.h>
+
 // Device resolution
 #define TEMP_9_BIT 0x1F  //  9 bit
 #define TEMP_10_BIT 0x3F // 10 bit
--- src/tempsensors.h
+++ src/tempsensors.h
@@ -24,10 +24,8 @@ SOFTWARE. */
 #define _TEMPSENSORS_H
 
 #include "config.h"
-#include "appconfig.h"
+
 #include <CircularBuffer.h>
-#include <OneWire.h>
-#include <DS18B20.h>
 
 // Temperature sensor definitions
 enum SensorList
